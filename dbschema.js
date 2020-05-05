7b0c91b7ac0b220e6a5e50e2f0a057bf0a2d9a0f
ntur5 <pressntr@gmail.com>
2020-05-04 21:47:36 -0600

    initial commit, include a route for getting all screams and a route for posting a screamusing firebase and express and node

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-05-05T03:35:29.886Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
